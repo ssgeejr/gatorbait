21e1398ce7755693d30239e1c97ad22fb7deb1a9
Steven Gee <1520789+ssgeejr@users.noreply.github.com>
2025-07-11 14:06:34 -0500

    updated the latest sql filds

--- sql/gatorbait-02.sql
+++ sql/gatorbait-02.sql
@@ -1,8 +1,10 @@
-select * from  compliance_audit_log
+select a.name,DATE_FORMAT(a.run_date, '%m/%d/%y') as lastLogin,a.numdays,b.value from  compliance_audit_log a, XREF b
+where a.type = b.key
 
 
 -- SELECT DATE_FORMAT(run_date, '%m/%d/%y') AS formatted_date from  compliance_audit_log
 
+-- TRUNCATE TABLE compliance_audit_log;
 
 
 
